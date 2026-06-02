98f6ef4f19f1ae2a801ed59bfb738a6cf2662da5
Luis Meier <meierluis@hotmail.de>
2026-06-02 17:57:37 +0200

    Waybar: Update network, nvidia, audio

--- config/waybar/scripts/nvidia_status.sh
+++ config/waybar/scripts/nvidia_status.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Check the line “Suspended” vs “Active” in supergfxctl output
+# Check the line "Suspended" vs "Active" in supergfxctl output
 state=$(cat /sys/bus/pci/devices/0000:01:00.0/power/runtime_status 2>/dev/null)
 
 if grep -q "suspended" <<<"$state"; then
-  printf '{"icon":" ","text":"","class":"suspended"}'
+  printf '{"text":"󰢮","class":"suspended"}'
 else
-  printf '{"icon":" ","text":"","class":"active"}'
+  printf '{"text":"󰢮","class":"active"}'
 fi
